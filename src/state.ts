ea848ed2789ab8b1428206d93a16d45fc399662b
inokawa <48897392+inokawa@users.noreply.github.com>
2022-11-05 22:54:21 +0900

    Replace useReducer with useState for performance

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,4 @@
-import { Reducer, useReducer } from "react";
+import { useCallback, useState } from "react";
 import {
   findStartIndexAfter,
   findStartIndexBefore,
@@ -23,119 +23,125 @@ type State = {
   _jump: ScrollJump;
 };
 
-const init = (elements: unknown[]): State => {
-  return {
-    _startIndex: 0,
-    _viewportSize: 0,
-    _cache: resetCache(elements),
-    _jump: { _start: 0, _end: 0 },
-  };
+type Actions =
+  | { _type: typeof RESET_CACHE; _elements: unknown[] }
+  | {
+      _type: typeof UPDATE_ITEM_SIZES;
+      _indexes: number[];
+      _sizes: number[];
+    }
+  | { _type: typeof UPDATE_VIEWPORT_SIZE; _size: number }
+  | {
+      _type: typeof HANDLE_ITEM_INTERSECTION;
+      _index: number;
+      _offset: number;
+    }
+  | { _type: typeof HANDLE_SCROLL; _offset: number };
+
+const reducer = (state: State, action: Actions, itemSize: number): State => {
+  switch (action._type) {
+    case RESET_CACHE: {
+      return {
+        ...state,
+        _cache: resetCache(action._elements, state._cache),
+      };
+    }
+    case UPDATE_ITEM_SIZES: {
+      const { _indexes: indexes, _sizes: sizes } = action;
+      if (indexes.every((index, i) => state._cache[index] === sizes[i]!)) {
+        return state;
+      }
+
+      let topJump = 0;
+      let bottomJump = 0;
+      indexes.forEach((index, i) => {
+        if (index < state._startIndex) {
+          topJump +=
+            sizes[i]! - resolveItemSize(state._cache[index]!, itemSize);
+        } else {
+          bottomJump +=
+            sizes[i]! - resolveItemSize(state._cache[index]!, itemSize);
+        }
+        state._cache[index] = sizes[i]!;
+      });
+
+      return {
+        ...state,
+        _jump: { _start: topJump, _end: bottomJump },
+      };
+    }
+    case UPDATE_VIEWPORT_SIZE: {
+      if (state._viewportSize === action._size) {
+        return state;
+      }
+      return {
+        ...state,
+        _viewportSize: action._size,
+      };
+    }
+    case HANDLE_ITEM_INTERSECTION: {
+      let startIndex: number;
+      if (action._offset <= 0) {
+        startIndex = findStartIndexAfter(
+          action._index,
+          max(0, -action._offset),
+          state._cache,
+          itemSize
+        );
+      } else {
+        startIndex = findStartIndexBefore(
+          action._index,
+          max(0, action._offset),
+          state._cache,
+          itemSize
+        );
+      }
+
+      if (startIndex === state._startIndex) {
+        return state;
+      }
+      return {
+        ...state,
+        _startIndex: startIndex,
+      };
+    }
+    case HANDLE_SCROLL: {
+      const startIndex = findStartIndexWithOffset(
+        action._offset,
+        state._cache,
+        itemSize
+      );
+      if (startIndex === state._startIndex) {
+        return state;
+      }
+      return {
+        ...state,
+        _startIndex: startIndex,
+      };
+    }
+  }
 };
 
-export const useVirtualState = (elements: unknown[], itemSize: number) => {
-  return useReducer<
-    Reducer<
-      State,
-      | { _type: typeof RESET_CACHE; _elements: unknown[] }
-      | {
-          _type: typeof UPDATE_ITEM_SIZES;
-          _indexes: number[];
-          _sizes: number[];
-        }
-      | { _type: typeof UPDATE_VIEWPORT_SIZE; _size: number }
-      | {
-          _type: typeof HANDLE_ITEM_INTERSECTION;
-          _index: number;
-          _offset: number;
-        }
-      | { _type: typeof HANDLE_SCROLL; _offset: number }
-    >,
-    unknown[]
-  >(
-    (state, action) => {
-      switch (action._type) {
-        case RESET_CACHE: {
-          return {
-            ...state,
-            _cache: resetCache(action._elements, state._cache),
-          };
-        }
-        case UPDATE_ITEM_SIZES: {
-          const { _indexes: indexes, _sizes: sizes } = action;
-          if (indexes.every((index, i) => state._cache[index] === sizes[i]!)) {
-            return state;
-          }
+// In React 18, useReducer causes rerender even if the state update didn't change anything.
+// So imitate useReducer with useState for performance.
+// https://github.com/facebook/react/pull/22445
+export const useVirtualState = (
+  elements: unknown[],
+  itemSize: number
+): [State, (action: Actions) => void] => {
+  const [state, setState] = useState(() => {
+    return {
+      _startIndex: 0,
+      _viewportSize: 0,
+      _cache: resetCache(elements),
+      _jump: { _start: 0, _end: 0 },
+    };
+  });
 
-          let topJump = 0;
-          let bottomJump = 0;
-          indexes.forEach((index, i) => {
-            if (index < state._startIndex) {
-              topJump +=
-                sizes[i]! - resolveItemSize(state._cache[index]!, itemSize);
-            } else {
-              bottomJump +=
-                sizes[i]! - resolveItemSize(state._cache[index]!, itemSize);
-            }
-            state._cache[index] = sizes[i]!;
-          });
-
-          return {
-            ...state,
-            _jump: { _start: topJump, _end: bottomJump },
-          };
-        }
-        case UPDATE_VIEWPORT_SIZE: {
-          if (state._viewportSize === action._size) {
-            return state;
-          }
-          return {
-            ...state,
-            _viewportSize: action._size,
-          };
-        }
-        case HANDLE_ITEM_INTERSECTION: {
-          let startIndex: number;
-          if (action._offset <= 0) {
-            startIndex = findStartIndexAfter(
-              action._index,
-              max(0, -action._offset),
-              state._cache,
-              itemSize
-            );
-          } else {
-            startIndex = findStartIndexBefore(
-              action._index,
-              max(0, action._offset),
-              state._cache,
-              itemSize
-            );
-          }
-
-          if (startIndex === state._startIndex) {
-            return state;
-          }
-          return {
-            ...state,
-            _startIndex: startIndex,
-          };
-        }
-        case HANDLE_SCROLL: {
-          const startIndex = findStartIndexWithOffset(
-            action._offset,
-            state._cache,
-            itemSize
-          );
-          if (startIndex === state._startIndex) {
-            return state;
-          }
-          return {
-            ...state,
-            _startIndex: startIndex,
-          };
-        }
-      }
-    },
-    elements,
-    init
-  );
+  return [
+    state,
+    useCallback((action: Actions) => {
+      setState((prev) => reducer(prev, action, itemSize));
+    }, []),
+  ];
 };
