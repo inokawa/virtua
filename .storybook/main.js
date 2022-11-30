bdd72e6edcccc53633f3fdd4c3ce9a279cb7cb3a
inokawa <48897392+inokawa@users.noreply.github.com>
2022-12-01 01:50:40 +0900

    Upgrade to storybook v7 alpha

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,28 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  framework: "@storybook/react",
+  addons: ["@storybook/addon-docs"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  async webpackFinal(config, options) {
+    config.module.rules = config.module.rules.filter(
+      (r) => !r.use?.[0]?.loader?.includes("babel-loader")
+    );
+    config.module.rules.unshift({
+      test: /\.(mjs|tsx?|jsx?)$/,
+      use: [
+        {
+          loader: "esbuild-loader",
+          options: {
+            loader: "tsx",
+            target: "es2018",
+            implementation: require("esbuild"),
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
