89f634143fd8fbb35b358c03f2de5198cb4939ce
inokawa <inokawa@users.noreply.github.com>
2023-03-27 16:04:17 +0000

    deploy: 5946692d5759b4da31e4aa9bbc6e14b2625dd5d6

---
+++ sb-manager/index.mjs
@@ -0,0 +1,1 @@
+import{Provider,Root,renderStorybookUI}from"./chunk-ZOUMJK5U.mjs";import"./chunk-VTYHZEJ7.mjs";import"./chunk-FGVEHHDQ.mjs";import"./chunk-WCYDYUGX.mjs";import"./chunk-DQ2XHBAE.mjs";import"./chunk-CGGDZUA2.mjs";import"./chunk-7NJ6NIF4.mjs";import"./chunk-KQFPYAFV.mjs";import"./chunk-JCTATS4D.mjs";export{Provider,Root,renderStorybookUI};
