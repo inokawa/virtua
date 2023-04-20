3456fc4fc425d8180df432afbde41a2bb2f4f4c2
inokawa <48897392+inokawa@users.noreply.github.com>
2023-04-21 01:51:37 +0900

    Replace @storybook/react-webpack5 with @storybook/react-vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,25 +5,7 @@ module.exports = {
   ],
   addons: ["@storybook/addon-docs"],
   framework: {
-    name: "@storybook/react-webpack5",
+    name: "@storybook/react-vite",
     options: {},
-  },
-  async webpackFinal(config, options) {
-    config.module.rules = config.module.rules.filter(
-      (r) => !r.use?.[0]?.loader?.includes("babel-loader")
-    );
-    config.module.rules.unshift({
-      test: /\.(mjs|tsx?|jsx?)$/,
-      use: [
-        {
-          loader: "esbuild-loader",
-          options: {
-            target: "es2018",
-            implementation: require("esbuild"),
-          },
-        },
-      ],
-    });
-    return config;
   },
 };
