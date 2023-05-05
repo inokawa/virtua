d1506ea1900b930bb0948821b2fb39ca9e04a443
inokawa <inokawa@users.noreply.github.com>
2023-05-05 06:57:34 +0000

    deploy: 505baf4bf7d15b1c00fa91252730f9d71ed5e800

---
+++ sb-addons/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var r=__STORYBOOKADDONS__,{addons:O,types:f,mockChannel:l}=__STORYBOOKADDONS__;O.setConfig({showPanel:!1});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
--- sb-addons/manager-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var r=__STORYBOOKADDONS__,{addons:O,types:f,mockChannel:l}=__STORYBOOKADDONS__;O.setConfig({showPanel:!1});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager-bundle.mjs.map
