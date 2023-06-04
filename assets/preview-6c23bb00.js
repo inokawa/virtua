b8f7680c0fb81df0b38c048328883f591e45ab87
inokawa <inokawa@users.noreply.github.com>
2023-06-04 06:09:12 +0000

    deploy: 8e03f8330a442c390157b4ccd7e68a07ab1b90f6

--- assets/preview-6c23bb00.js
+++
@@ -1,2 +0,0 @@
-import{j as o}from"./jsx-runtime-c3d7f245.js";import"./index-c6dae603.js";const t={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics","advanced","comparisons"]}}},a=[r=>o("div",{style:{background:"whitesmoke",width:"100vw",height:"100vh"},children:o(r,{})})];export{a as decorators,t as parameters};
-//# sourceMappingURL=preview-6c23bb00.js.map
