3822d60f2d99d3c33a370a80e8e8b1ff63234b25
inokawa <inokawa@users.noreply.github.com>
2023-07-28 09:25:31 +0000

    deploy: f36e5b7db19f2cf6952eed85cfe536369e0ca25b

---
+++ sb-addons/storybook-1/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var i=__STORYBOOKAPI__,{ActiveTabs:l,Consumer:m,ManagerContext:h,Provider:p,addons:r,combineParameters:y,controlOrMetaKey:d,controlOrMetaSymbol:b,eventMatchesShortcut:O,eventToShortcut:T,isMacLike:A,isShortcutTaken:_,keyToSymbol:g,merge:P,mockChannel:k,optionOrAltSymbol:M,shortcutMatchesShortcut:C,shortcutToHumanString:v,types:f,useAddonState:x,useArgTypes:K,useArgs:B,useChannel:G,useGlobalTypes:I,useGlobals:R,useParameter:Y,useSharedState:H,useStoryPrepared:L,useStorybookApi:j,useStorybookState:q}=__STORYBOOKAPI__;r.setConfig({panelPosition:"right"});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
