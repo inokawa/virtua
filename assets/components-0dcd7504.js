b960c1cb1a3dfb188ab9a176fd8319069d0b7691
inokawa <inokawa@users.noreply.github.com>
2023-07-29 16:47:03 +0000

    deploy: bf1a6244647771c5cd9aa2d7905d692e02dab3cd

---
+++ assets/components-0dcd7504.js
@@ -0,0 +1,30 @@
+import{a as e,F as t,j as i}from"./jsx-runtime-c3d7f245.js";const r=()=>e(t,{children:[i("div",{style:{height:100,display:"flex",alignItems:"center",justifyContent:"center",background:"white"},children:i("span",{className:"loader"})}),i("style",{children:`
+      .loader {
+        width: 48px;
+        height: 48px;
+        border-radius: 50%;
+        position: relative;
+        animation: rotate 1s linear infinite
+      }
+      .loader::before {
+        content: "";
+        box-sizing: border-box;
+        position: absolute;
+        inset: 0px;
+        border-radius: 50%;
+        border: 5px solid #ccc;
+        animation: prixClipFix 2s linear infinite ;
+      }
+  
+      @keyframes rotate {
+        100%   {transform: rotate(360deg)}
+      }
+  
+      @keyframes prixClipFix {
+          0%   {clip-path:polygon(50% 50%,0 0,0 0,0 0,0 0,0 0)}
+          25%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 0,100% 0,100% 0)}
+          50%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,100% 100%,100% 100%)}
+          75%  {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 100%)}
+          100% {clip-path:polygon(50% 50%,0 0,100% 0,100% 100%,0 100%,0 0)}
+      }`})]});export{r as S};
+//# sourceMappingURL=components-0dcd7504.js.map
