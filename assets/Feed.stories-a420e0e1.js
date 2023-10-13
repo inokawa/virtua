d12b4e504f8bceefd16d3ccbb90491d5a8ae8b91
inokawa <inokawa@users.noreply.github.com>
2023-10-13 04:28:36 +0000

    deploy: eef160cb52819ed7d7d05683a22ef0b60602ebc6

--- assets/Feed.stories-a420e0e1.js
+++ assets/Feed.stories-a420e0e1.js
@@ -1,4 +1,4 @@
-import{j as d,a as T}from"./jsx-runtime-c3d7f245.js";import{r as t}from"./index-c6dae603.js";import{S as w,r as D,d as L}from"./common-4271bb5e.js";import{f as F}from"./index-cb482739.js";import{V as C}from"./VList-a7ebc38e.js";import"./Viewport-4a611d8b.js";import"./ListItem-0e2c78d2.js";import"./index-eb008d06.js";const J={component:C},b={borderTop:"solid 1px #ccc",background:"#fff",padding:32,paddingTop:48,paddingBottom:48,whiteSpace:"pre-wrap"},k=({content:s})=>T("div",{style:b,children:[s," "]}),c={name:"Feed",render:()=>{const s=t.useRef(0),E=()=>Math.random()>.2?{type:"text",id:s.current++,value:F.lorem.paragraphs(Math.floor(Math.random()*10)+1)}:{type:"image",id:s.current++,src:F.image.url()},o=e=>D(e,E),[v,H]=t.useState(!1),[B,x]=t.useState(!1),[M,_]=t.useState(!1),f=async(e=!1)=>{H(e);const a=e?x:_;a(!0),await L(1e3),a(!1)},m=t.useRef(null),i=30,[r,l]=t.useState(()=>o(i*2)),O=t.useMemo(()=>r.map(e=>d(k,{content:e.type==="image"?d("img",{src:e.src}):e.value},e.id)),[r]),h=10,n=r.length,g=t.useRef(-1),p=t.useRef(-1),y=t.useRef(!1);return t.useEffect(()=>{var e;(e=m.current)==null||e.scrollToIndex(r.length/2+1),y.current=!0},[]),T(C,{ref:m,style:{flex:1},shift:!!v,onRangeChange:async(e,a)=>{y.current&&(a+h>n&&p.current<n?(p.current=n,await f(),l(u=>[...u,...o(i)])):e-h<0&&g.current<n&&(g.current=n,await f(!0),l(u=>[...o(i).reverse(),...u])))},children:[O,d(w,{style:M?void 0:{visibility:"hidden"}},"foot")]})}};var S,I,R;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
+import{j as d,a as T}from"./jsx-runtime-c3d7f245.js";import{r as t}from"./index-c6dae603.js";import{S as w,r as D,d as L}from"./common-4271bb5e.js";import{f as F}from"./index-cb482739.js";import{V as C}from"./VList-a7ebc38e.js";import"./Viewport-4a611d8b.js";import"./ListItem-0e2c78d2.js";import"./index-eb008d06.js";const G={component:C},b={borderTop:"solid 1px #ccc",background:"#fff",padding:32,paddingTop:48,paddingBottom:48,whiteSpace:"pre-wrap"},k=({content:s})=>T("div",{style:b,children:[s," "]}),c={name:"Feed",render:()=>{const s=t.useRef(0),E=()=>Math.random()>.2?{type:"text",id:s.current++,value:F.lorem.paragraphs(Math.floor(Math.random()*10)+1)}:{type:"image",id:s.current++,src:F.image.url()},i=e=>D(e,E),[v,x]=t.useState(!1),[B,H]=t.useState(!1),[M,_]=t.useState(!1),f=async(e=!1)=>{x(e);const a=e?H:_;a(!0),await L(1e3),a(!1)},m=t.useRef(null),o=30,[r,l]=t.useState(()=>i(o*2)),O=t.useMemo(()=>r.map(e=>d(k,{content:e.type==="image"?d("img",{src:e.src,style:{maxWidth:"100%"}}):e.value},e.id)),[r]),h=10,n=r.length,g=t.useRef(-1),p=t.useRef(-1),y=t.useRef(!1);return t.useEffect(()=>{var e;(e=m.current)==null||e.scrollToIndex(r.length/2+1),y.current=!0},[]),T(C,{ref:m,style:{flex:1},shift:!!v,onRangeChange:async(e,a)=>{y.current&&(a+h>n&&p.current<n?(p.current=n,await f(),l(u=>[...u,...i(o)])):e-h<0&&g.current<n&&(g.current=n,await f(!0),l(u=>[...i(o).reverse(),...u])))},children:[O,d(w,{style:M?void 0:{visibility:"hidden"}},"foot")]})}};var S,I,R;c.parameters={...c.parameters,docs:{...(S=c.parameters)==null?void 0:S.docs,source:{originalSource:`{
   name: "Feed",
   render: () => {
     const id = useRef(0);
@@ -28,7 +28,9 @@ import{j as d,a as T}from"./jsx-runtime-c3d7f245.js";import{r as t}from"./index-
     const ref = useRef<VListHandle>(null);
     const ITEM_BATCH_COUNT = 30;
     const [items, setItems] = useState(() => createItems(ITEM_BATCH_COUNT * 2));
-    const elements = useMemo(() => items.map(d => <Item key={d.id} content={d.type === "image" ? <img src={d.src} /> : d.value} />), [items]);
+    const elements = useMemo(() => items.map(d => <Item key={d.id} content={d.type === "image" ? <img src={d.src} style={{
+      maxWidth: "100%"
+    }} /> : d.value} />), [items]);
     const THRESHOLD = 10;
     const count = items.length;
     const startFetchedCountRef = useRef(-1);
@@ -63,5 +65,5 @@ import{j as d,a as T}from"./jsx-runtime-c3d7f245.js";import{r as t}from"./index-
       }} />
       </VList>;
   }
-}`,...(R=(I=c.parameters)==null?void 0:I.docs)==null?void 0:R.source}}};const K=["Default"];export{c as Default,K as __namedExportsOrder,J as default};
-//# sourceMappingURL=Feed.stories-5b09b051.js.map
+}`,...(R=(I=c.parameters)==null?void 0:I.docs)==null?void 0:R.source}}};const J=["Default"];export{c as Default,J as __namedExportsOrder,G as default};
+//# sourceMappingURL=Feed.stories-a420e0e1.js.map
