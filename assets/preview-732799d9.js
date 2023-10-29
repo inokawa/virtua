b105f244503644cd9b817554845c440e4ddd2df2
inokawa <inokawa@users.noreply.github.com>
2023-10-29 05:37:57 +0000

    deploy: a6aa844f95b1c290d0e0f1a180fea6b0ce0a5f68

--- assets/preview-732799d9.js
+++
@@ -1,2 +0,0 @@
-({BASE_URL:"./",MODE:"production",DEV:!1,PROD:!0,SSR:!1,STORYBOOK:"true"}).STORYBOOK_RTL&&(document.documentElement.dir="rtl");const e={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["VList","VGrid","WVList"],"advanced","comparisons"]}}};export{e as parameters};
-//# sourceMappingURL=preview-732799d9.js.map
