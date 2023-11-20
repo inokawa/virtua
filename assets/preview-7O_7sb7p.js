5c62b6bb9d9b28c0518296ffe516b884dc61998a
inokawa <inokawa@users.noreply.github.com>
2023-11-20 00:38:49 +0000

    deploy: 4869d97c29f8f9f0988a9bf3a29d9c839d61972d

---
+++ assets/preview-7O_7sb7p.js
@@ -0,0 +1,4 @@
+var t={BASE_URL:"./",MODE:"production",DEV:!1,PROD:!0,SSR:!1,STORYBOOK:"true"};t.STORYBOOK_RTL&&(document.documentElement.dir="rtl");if(t.STORYBOOK_E2E){const e=document.createElement("style");e.appendChild(document.createTextNode(`* { 
+        pointer-events: auto !important;
+      }`)),document.head.appendChild(e)}const o={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["VList","WVList","VGrid"],"advanced","comparisons"]}}};export{o as parameters};
+//# sourceMappingURL=preview-7O_7sb7p.js.map
--- assets/preview-9I-pNmc-.js
+++
@@ -1,2 +0,0 @@
-var e={BASE_URL:"./",MODE:"production",DEV:!1,PROD:!0,SSR:!1,STORYBOOK:"true"};e.STORYBOOK_RTL&&(document.documentElement.dir="rtl");const t={actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["VList","WVList","VGrid"],"advanced","comparisons"]}}};export{t as parameters};
-//# sourceMappingURL=preview-9I-pNmc-.js.map
