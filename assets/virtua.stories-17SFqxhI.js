d6f81537969e959e3a83c7ae129c1288dbe652f0
inokawa <inokawa@users.noreply.github.com>
2023-11-25 04:22:26 +0000

    deploy: bb4c87eb3fe870715038df4c33873015e9210469

--- assets/virtua.stories-17SFqxhI.js
+++ assets/virtua.stories-17SFqxhI.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y}from"./common-opS2vWa9.js";import{V as n}from"./virtua-9w_xnovx.js";import"./index-G6kSzgdV.js";import"./index-xBZDQ2qw.js";const S={component:n,decorators:[r=>e("div",{style:{height:"100vh"},children:e(r,{})})]},t={render:()=>e(n,{count:1e4,Component:T})},o={render:()=>e(n,{count:1e4,Component:U})},s={render:()=>e(n,{count:1e4,Component:l})},a={render:()=>e(n,{count:1e6,Component:y})};var c,m,i;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y}from"./common-opS2vWa9.js";import{V as n}from"./virtua-FT8yW3FI.js";import"./index-G6kSzgdV.js";import"./index-xBZDQ2qw.js";const S={component:n,decorators:[r=>e("div",{style:{height:"100vh"},children:e(r,{})})]},t={render:()=>e(n,{count:1e4,Component:T})},o={render:()=>e(n,{count:1e4,Component:U})},s={render:()=>e(n,{count:1e4,Component:l})},a={render:()=>e(n,{count:1e6,Component:y})};var c,m,i;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
   render: () => {
     const ROW_COUNT = 10000;
     return <VirtuaList count={ROW_COUNT} Component={ItemWithRenderCount} />;
@@ -19,4 +19,4 @@ import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y
     return <VirtuaList count={ROW_COUNT} Component={SimpleItem} />;
   }
 }`,...(N=(W=a.parameters)==null?void 0:W.docs)==null?void 0:N.source}}};const V=["RenderCount","DynamicHeight","HeavyComponent","OneMillion"];export{o as DynamicHeight,s as HeavyComponent,a as OneMillion,t as RenderCount,V as __namedExportsOrder,S as default};
-//# sourceMappingURL=virtua.stories-bDk3_B-r.js.map
+//# sourceMappingURL=virtua.stories-17SFqxhI.js.map
