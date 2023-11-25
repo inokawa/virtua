ae35a72802de99adbf08618a9156cb7c2689862a
inokawa <inokawa@users.noreply.github.com>
2023-11-25 04:35:44 +0000

    deploy: 4c089dc913eea44d7663b3d6eb37d89e926460b3

--- assets/react-window.stories-rwB8zGzq.js
+++ assets/react-window.stories-rwB8zGzq.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y}from"./common-opS2vWa9.js";import{R as r}from"./react-window-WB1KO_lz.js";import"./index-G6kSzgdV.js";import"./AutoSizer-6KFUA3PF.js";import"./inherits-dAtQwFGf.js";import"./defineProperty-G_ESCvJ5.js";import"./assertThisInitialized-4q6YPdh3.js";import"./setPrototypeOf-ahVgEFUp.js";import"./extends-dGVwEr9R.js";import"./inheritsLoose-fS6oVJzb.js";import"./memoize-one.esm-hqe5SRxC.js";const M={component:r,decorators:[n=>e("div",{style:{height:"100vh"},children:e(n,{})})]},o={render:()=>e(r,{count:1e4,Component:T})},t={render:()=>e(r,{count:1e4,Component:U})},s={render:()=>e(r,{count:1e4,Component:l})},a={render:()=>e(r,{count:1e6,Component:y})};var c,m,i;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
+import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y}from"./common-opS2vWa9.js";import{R as r}from"./react-window-WTYPQtKe.js";import"./index-G6kSzgdV.js";import"./AutoSizer-6KFUA3PF.js";import"./inherits-dAtQwFGf.js";import"./defineProperty-G_ESCvJ5.js";import"./assertThisInitialized-4q6YPdh3.js";import"./setPrototypeOf-ahVgEFUp.js";import"./extends-dGVwEr9R.js";import"./inheritsLoose-fS6oVJzb.js";import"./memoize-one.esm-hqe5SRxC.js";const M={component:r,decorators:[n=>e("div",{style:{height:"100vh"},children:e(n,{})})]},o={render:()=>e(r,{count:1e4,Component:T})},t={render:()=>e(r,{count:1e4,Component:U})},s={render:()=>e(r,{count:1e4,Component:l})},a={render:()=>e(r,{count:1e6,Component:y})};var c,m,i;o.parameters={...o.parameters,docs:{...(c=o.parameters)==null?void 0:c.docs,source:{originalSource:`{
   render: () => {
     const ROW_COUNT = 10000;
     return <ReactWindowList count={ROW_COUNT} Component={ItemWithRenderCount} />;
@@ -19,4 +19,4 @@ import{j as e}from"./jsx-runtime-KhpQ_ow-.js";import{I as T,D as U,H as l,S as y
     return <ReactWindowList count={ROW_COUNT} Component={SimpleItem} />;
   }
 }`,...(N=(_=a.parameters)==null?void 0:_.docs)==null?void 0:N.source}}};const E=["RenderCount","DynamicHeight","HeavyComponent","OneMillion"];export{t as DynamicHeight,s as HeavyComponent,a as OneMillion,o as RenderCount,E as __namedExportsOrder,M as default};
-//# sourceMappingURL=react-window.stories-2UBJpTTX.js.map
+//# sourceMappingURL=react-window.stories-rwB8zGzq.js.map
