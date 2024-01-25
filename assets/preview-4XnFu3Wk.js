34650726586cbe310355acb27af1d0e7f71ee9bd
inokawa <inokawa@users.noreply.github.com>
2024-01-25 12:09:13 +0000

    deploy: ea6c095e3b6b97209ad4360de0dffdb53c797349

---
+++ assets/preview-4XnFu3Wk.js
@@ -0,0 +1,3 @@
+var t={STORYBOOK_DEPLOY:"1",BASE_URL:"./",MODE:"production",DEV:!1,PROD:!0,SSR:!1,STORYBOOK:"true"};t.STORYBOOK_RTL&&(document.documentElement.dir="rtl");if(t.STORYBOOK_E2E){const e=document.createElement("style");e.appendChild(document.createTextNode(`* { 
+        pointer-events: auto !important;
+      }`)),document.head.appendChild(e)}const r={parameters:{actions:{argTypesRegex:"^on[A-Z].*"},layout:"fullscreen",options:{storySort:{order:["basics",["VList","Virtualizer","WindowVirtualizer","VGrid"],"advanced","comparisons"]}}}};export{r as default};
