536c6eb356c9ef79acf2d2e26b53ecb9e35effa8
inokawa <48897392+inokawa@users.noreply.github.com>
2024-01-28 23:17:33 +0900

    Update vue stories

--- stories/vue/Default.vue
+++ stories/vue/Default.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { VList } from '../../src/vue'
 
 const sizes = [20, 40, 180, 77];
 const createItem = (i: number) => ({ index: i, size: sizes[i % 4] + 'px' })
 
-const data = ref(Array.from({ length: 1000 }).map((_, i) => createItem(i)));
+const data = Array.from({ length: 1000 }).map((_, i) => createItem(i));
 
 </script>
 
