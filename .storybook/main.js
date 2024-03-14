d64d8744ef4dd001588696e194892bcc162b3541
inokawa <48897392+inokawa@users.noreply.github.com>
2024-03-14 11:32:52 +0900

    Migrate to storybook v8

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,39 +10,40 @@ export default process.env.STORYBOOK_VUE
         options: {},
       },
       viteFinal: async (config) => {
+        const { default: vue } = await import("@vitejs/plugin-vue");
         const { default: vueJsx } = await import("@vitejs/plugin-vue-jsx");
 
         return mergeConfig(config, {
-          plugins: [vueJsx({ optimize: true })],
+          plugins: [vue(), vueJsx({ optimize: true })],
         });
       },
     }
   : process.env.STORYBOOK_SOLID
-  ? {
-      stories: ["../stories/solid/**/*.stories.@(js|jsx|ts|tsx)"],
-      addons: ["@storybook/addon-storysource"],
-      framework: {
-        name: "storybook-solidjs-vite",
-        options: {},
-      },
-    }
-  : {
-      stories: ["../stories/react/**/*.stories.@(js|jsx|ts|tsx)"],
-      addons: ["@storybook/addon-storysource"],
-      framework: {
-        name: "@storybook/react-vite",
-        options: {},
-      },
-      ...(process.env.STORYBOOK_DEPLOY && {
-        refs: {
-          vue: {
-            title: "Vue",
-            url: "/virtua/vue",
-          },
-          solid: {
-            title: "Solid",
-            url: "/virtua/solid",
-          },
+    ? {
+        stories: ["../stories/solid/**/*.stories.@(js|jsx|ts|tsx)"],
+        addons: ["@storybook/addon-storysource"],
+        framework: {
+          name: "storybook-solidjs-vite",
+          options: {},
         },
-      }),
-    };
+      }
+    : {
+        stories: ["../stories/react/**/*.stories.@(js|jsx|ts|tsx)"],
+        addons: ["@storybook/addon-storysource"],
+        framework: {
+          name: "@storybook/react-vite",
+          options: {},
+        },
+        ...(process.env.STORYBOOK_DEPLOY && {
+          refs: {
+            vue: {
+              title: "Vue",
+              url: "/virtua/vue",
+            },
+            solid: {
+              title: "Solid",
+              url: "/virtua/solid",
+            },
+          },
+        }),
+      };
