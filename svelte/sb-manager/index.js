093844fca40e2706bff96da3e2eb7f953b5e58b8
inokawa <inokawa@users.noreply.github.com>
2024-05-14 12:19:56 +0000

    deploy: 467f836ec1a14c73333420ab89b20d075049760a

---
+++ svelte/sb-manager/index.js
@@ -0,0 +1,1 @@
+import{Provider,Root,renderStorybookUI}from"./chunk-GFFGH77H.js";import"./chunk-KCHUFUTG.js";import"./chunk-XCO5HRLK.js";import"./chunk-FEE35O7J.js";import"./chunk-S4VOIVUE.js";import"./chunk-XP3HGWTR.js";export{Provider,Root,renderStorybookUI};
