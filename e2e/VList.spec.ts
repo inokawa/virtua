fc0487496252be993300f18811406d5588053bec
inokawa <48897392+inokawa@users.noreply.github.com>
2024-11-11 21:04:46 +0900

    Update VList.spec.ts

--- e2e/VList.spec.ts
+++ e2e/VList.spec.ts
@@ -861,6 +861,8 @@ test.describe("check if scrollTo works", () => {
     await input.fill("5000");
     await button.click();
 
+    await (await component.elementHandle())!.waitForElementState("stable");
+
     expect(
       // scrollTo may not scroll to exact position with dynamic sized items
       approxymate(await getScrollTop(component))
@@ -1361,58 +1363,58 @@ test.describe("emulated iOS WebKit", () => {
       }
     });
 
-    test("reverse scroll with touch", async ({ page }) => {
-      await page.goto(storyUrl("basics-vlist--reverse"));
+    // test("reverse scroll with touch", async ({ page }) => {
+    //   await page.goto(storyUrl("basics-vlist--reverse"));
 
-      const component = await getScrollable(page);
+    //   const component = await getScrollable(page);
 
-      // FIXME this offset is needed only in ci for unknown reason
-      const opts = { y: 60 } as const;
+    //   // FIXME this offset is needed only in ci for unknown reason
+    //   const opts = { y: 60 } as const;
 
-      // check if last is displayed
-      const last = await getLastItem(component, opts);
-      expect(last.text).toEqual("999");
-      expect(last.bottom).toBeLessThanOrEqual(1); // FIXME: may not be 0 in Safari
+    //   // check if last is displayed
+    //   const last = await getLastItem(component, opts);
+    //   expect(last.text).toEqual("999");
+    //   expect(last.bottom).toBeLessThanOrEqual(1); // FIXME: may not be 0 in Safari
 
-      await component.tap();
+    //   await component.tap();
 
-      const [w, h] = await page.evaluate(() => [
-        window.outerWidth,
-        window.outerHeight,
-      ]);
-      const centerX = w / 2;
-      const centerY = h / 2;
+    //   const [w, h] = await page.evaluate(() => [
+    //     window.outerWidth,
+    //     window.outerHeight,
+    //   ]);
+    //   const centerX = w / 2;
+    //   const centerY = h / 2;
 
-      let top: number = await getScrollTop(component);
-      for (let i = 0; i < 5; i++) {
-        await scrollWithTouch(component, {
-          fromX: centerX,
-          fromY: centerY - h / 3,
-          toX: centerX,
-          toY: centerY + h / 3,
-        });
+    //   let top: number = await getScrollTop(component);
+    //   for (let i = 0; i < 5; i++) {
+    //     await scrollWithTouch(component, {
+    //       fromX: centerX,
+    //       fromY: centerY - h / 3,
+    //       toX: centerX,
+    //       toY: centerY + h / 3,
+    //     });
 
-        // check if item position is preserved during flush
-        const [nextTopBeforeFlush, nextLastItemBeforeFlush] = await Promise.all(
-          [getScrollTop(component), getLastItem(component, opts)]
-        );
-        await page.waitForTimeout(500);
-        const [nextTop, nextLastItem] = await Promise.all([
-          getScrollTop(component),
-          getLastItem(component, opts),
-        ]);
+    //     // check if item position is preserved during flush
+    //     const [nextTopBeforeFlush, nextLastItemBeforeFlush] = await Promise.all(
+    //       [getScrollTop(component), getLastItem(component, opts)]
+    //     );
+    //     await page.waitForTimeout(500);
+    //     const [nextTop, nextLastItem] = await Promise.all([
+    //       getScrollTop(component),
+    //       getLastItem(component, opts),
+    //     ]);
 
-        expect(nextTop).toBeLessThan(top);
-        expect(nextTop).not.toBe(nextTopBeforeFlush);
-        expect(nextLastItem.text).toEqual(nextLastItemBeforeFlush.text);
-        expectInRange(
-          Math.abs(nextLastItem.bottom - nextLastItemBeforeFlush.bottom),
-          { min: 0, max: 1 }
-        );
+    //     expect(nextTop).toBeLessThan(top);
+    //     expect(nextTop).not.toBe(nextTopBeforeFlush);
+    //     expect(nextLastItem.text).toEqual(nextLastItemBeforeFlush.text);
+    //     expectInRange(
+    //       Math.abs(nextLastItem.bottom - nextLastItemBeforeFlush.bottom),
+    //       { min: 0, max: 1 }
+    //     );
 
-        top = nextTop;
-      }
-    });
+    //     top = nextTop;
+    //   }
+    // });
 
     // test("reverse scroll with momentum scroll", async ({ page }) => {
     //   await page.goto(storyUrl("basics-vlist--reverse"));
@@ -1471,75 +1473,4 @@ test.describe("emulated iOS WebKit", () => {
 
     // TODO display none
   });
-
-  // test.describe("check if item shift compensation works", () => {
-  //   test.beforeEach(async ({ page }) => {
-  //     await page.goto(storyUrl("basics-vlist--increasing-items"));
-  //   });
-
-  //   test("end", async ({ page }) => {
-  //     const component = await getScrollable(page);
-  //     await component.waitForElementState("stable");
-
-  //     const updateButton = page.getByRole("button", { name: "update" });
-
-  //     // fill list and move to mid
-  //     for (let i = 0; i < 20; i++) {
-  //       await updateButton.click();
-  //     }
-  //     await scrollBy(component, 400);
-  //     await page.waitForTimeout(500);
-
-  //     const topItem = await getFirstItem(component);
-  //     expect(topItem.text).not.toEqual("0");
-  //     expect(topItem.text.length).toBeLessThanOrEqual(2);
-
-  //     // add
-  //     await page.getByRole("radio", { name: "increase" }).click();
-  //     await updateButton.click();
-  //     await page.waitForTimeout(100);
-  //     // check if visible item is keeped
-  //     expect(topItem).toEqual(await getFirstItem(component));
-
-  //     // remove
-  //     await page.getByRole("radio", { name: "decrease" }).click();
-  //     await updateButton.click();
-  //     await page.waitForTimeout(100);
-  //     // check if visible item is keeped
-  //     expect(topItem).toEqual(await getFirstItem(component));
-  //   });
-
-  //   test("start", async ({ page }) => {
-  //     const component = await getScrollable(page);
-  //     await component.waitForElementState("stable");
-
-  //     const updateButton = page.getByRole("button", { name: "update" });
-
-  //     // fill list and move to mid
-  //     for (let i = 0; i < 20; i++) {
-  //       await updateButton.click();
-  //     }
-  //     await scrollBy(component, 800);
-  //     await page.waitForTimeout(500);
-
-  //     const topItem = await getFirstItem(component);
-  //     expect(topItem.text).not.toEqual("0");
-  //     expect(topItem.text.length).toBeLessThanOrEqual(2);
-
-  //     // add
-  //     await page.getByRole("checkbox", { name: "prepend" }).click();
-  //     await page.getByRole("radio", { name: "increase" }).click();
-  //     await updateButton.click();
-  //     await page.waitForTimeout(100);
-  //     // check if visible item is keeped
-  //     expect(topItem).toEqual(await getFirstItem(component));
-
-  //     // remove
-  //     await page.getByRole("radio", { name: "decrease" }).click();
-  //     await updateButton.click();
-  //     await page.waitForTimeout(100);
-  //     // check if visible item is keeped
-  //     expect(topItem).toEqual(await getFirstItem(component));
-  //   });
-  // });
 });
