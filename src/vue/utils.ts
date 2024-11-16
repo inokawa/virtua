01d57e50a58f537130cd777d494fc8eaa34160c6
inokawa <48897392+inokawa@users.noreply.github.com>
2024-11-17 01:39:11 +0900

    Optimize Vue virtualizer with computed

--- src/vue/utils.ts
+++ src/vue/utils.ts
@@ -1,5 +1,6 @@
 import { VNode } from "vue";
 import { NULL } from "../core/utils";
+import { ItemsRange } from "../core/types";
 
 /**
  * @internal
@@ -7,4 +8,11 @@ import { NULL } from "../core/utils";
 export const getKey = (e: VNode, i: number): Exclude<VNode["key"], null> => {
   const key = e.key;
   return key != NULL ? key : "_" + i;
+};
+
+/**
+ * @internal
+ */
+export const isSameRange = (prev: ItemsRange, next: ItemsRange): boolean => {
+  return prev[0] === next[0] && prev[1] === next[1];
 };
