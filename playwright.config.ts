2c8a69ae346f8ba8002303b4050904d0ee76077c
inokawa <48897392+inokawa@users.noreply.github.com>
2024-11-24 14:29:41 +0900

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,27 +51,11 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
       grepInvert: new RegExp(String.raw`${IOS_SPECS}`),
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
     {
       name: "Mobile Safari",
       use: { ...devices["iPhone 13"] },
       grep: new RegExp(String.raw`${IOS_SPECS}`),
     },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
