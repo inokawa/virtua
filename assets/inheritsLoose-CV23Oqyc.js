4bb9184596d1fdec4ed6dbe832941fc78ee624d7
inokawa <inokawa@users.noreply.github.com>
2024-11-30 00:45:37 +0000

    deploy: 42e414d785e5a71cbc0de05c26cefdaf5480b7ca

---
+++ assets/inheritsLoose-CV23Oqyc.js
@@ -0,0 +1,1 @@
+import{b as e}from"./assertThisInitialized-DUTgmdVG.js";function p(o,t){o.prototype=Object.create(t.prototype),o.prototype.constructor=o,e(o,t)}export{p as _};
--- assets/inheritsLoose-D3k9JupM.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./assertThisInitialized-Vk491Nbt.js";function p(o,t){o.prototype=Object.create(t.prototype),o.prototype.constructor=o,e(o,t)}export{p as _};
