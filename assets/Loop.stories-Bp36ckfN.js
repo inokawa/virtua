3d3d18210fdbb8e0024487e5b98ceb86c6d2b35a
inokawa <inokawa@users.noreply.github.com>
2024-12-05 00:26:23 +0000

    deploy: 12af5e5cf3333ee2c6e868ce528e67113d4d4f6a

---
+++ assets/Loop.stories-Bp36ckfN.js
@@ -0,0 +1,59 @@
+import{j as l}from"./jsx-runtime-DR9Q75dM.js";import{r as t}from"./index-DRjF_FHU.js";import{r as _}from"./common-B4Hkentz.js";import{V as m}from"./VList-BAPMNa19.js";import"./Virtualizer-CaPS7AGl.js";import"./useRerender-BvLS7D9d.js";import"./useChildren-Dw0sRjzb.js";import"./index-uWwxbAOW.js";const x={component:m},c={name:"Loop",render:()=>{const a=t.useRef(0),L=()=>({id:a.current++}),u=e=>_(e,L),s=t.useRef(null),[O,o]=t.useState(()=>u(200)),i=t.useRef(-1),n=t.useRef(!1);return t.useLayoutEffect(()=>{var e;(e=s.current)==null||e.scrollToIndex(200/2)},[]),l.jsx(m,{ref:s,style:{flex:1},shift:n.current,onScroll:e=>{if(n.current=e-i.current<0,i.current=e,!!s.current){if(e<100){const d=n.current;o(r=>[...u(200/4),...r]),setTimeout(()=>{n.current=!d,o(r=>[...r.slice(0,200*3/4)])})}else if(s.current.scrollSize-s.current.viewportSize-e<100){const d=n.current;o(r=>[...r,...u(200/4)]),setTimeout(()=>{n.current=!d,o(r=>[...r.slice(200/4)])})}}},children:O.map(e=>l.jsx("div",{style:{height:100,backgroundColor:e.id%100===0?"darkgray":e.id%10===0?"smokegray":"white",borderBottom:"solid 1px #ccc"},children:e.id},e.id))})}};var T,p,f;c.parameters={...c.parameters,docs:{...(T=c.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  name: "Loop",
+  render: () => {
+    const TOTAL_LENGTH = 200;
+    const OFFSET_TO_BOUND = 100;
+    const id = useRef(0);
+    const createItem = () => {
+      return {
+        id: id.current++
+      };
+    };
+    const createItems = (num: number) => range(num, createItem);
+    const ref = useRef<VListHandle>(null);
+    const [items, setItems] = useState(() => createItems(TOTAL_LENGTH));
+    const prevScrollOffset = useRef(-1);
+    const shouldPrepend = useRef(false);
+    useLayoutEffect(() => {
+      ref.current?.scrollToIndex(TOTAL_LENGTH / 2);
+    }, []);
+    return <VList ref={ref} style={{
+      flex: 1
+    }} shift={shouldPrepend.current} onScroll={offset => {
+      shouldPrepend.current = offset - prevScrollOffset.current < 0;
+      prevScrollOffset.current = offset;
+      if (!ref.current) return;
+      if (offset < OFFSET_TO_BOUND) {
+        const currentShouldPrepend = shouldPrepend.current;
+        setItems(prev => {
+          return [...createItems(TOTAL_LENGTH / 4), ...prev];
+        });
+        setTimeout(() => {
+          shouldPrepend.current = !currentShouldPrepend;
+          setItems(prev => {
+            return [...prev.slice(0, TOTAL_LENGTH * 3 / 4)];
+          });
+        });
+      } else if (ref.current.scrollSize - ref.current.viewportSize - offset < OFFSET_TO_BOUND) {
+        const currentShouldPrepend = shouldPrepend.current;
+        setItems(prev => {
+          return [...prev, ...createItems(TOTAL_LENGTH / 4)];
+        });
+        setTimeout(() => {
+          shouldPrepend.current = !currentShouldPrepend;
+          setItems(prev => {
+            return [...prev.slice(TOTAL_LENGTH / 4)];
+          });
+        });
+      }
+    }}>
+        {items.map(d => <div key={d.id} style={{
+        height: 100,
+        backgroundColor: d.id % 100 === 0 ? "darkgray" : d.id % 10 === 0 ? "smokegray" : "white",
+        borderBottom: "solid 1px #ccc"
+      }}>
+            {d.id}
+          </div>)}
+      </VList>;
+  }
+}`,...(f=(p=c.parameters)==null?void 0:p.docs)==null?void 0:f.source}}};const F=["Default"];export{c as Default,F as __namedExportsOrder,x as default};
