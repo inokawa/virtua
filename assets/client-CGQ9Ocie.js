f47185383c364ca5be1a8abcc6ccfa321311a971
inokawa <inokawa@users.noreply.github.com>
2025-05-24 12:30:30 +0000

    deploy: 77d25eb0cb7cbf10e3c0c8a8e99a4bcea31f734b

--- assets/client-CGQ9Ocie.js
+++
@@ -1,1 +0,0 @@
-import{r}from"./index-uWwxbAOW.js";var t,a,o=r;a=o.createRoot,t=o.hydrateRoot;export{a as c,t as h};
---
+++ assets/react-virtualized.stories-BkIF2rv4.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-DR9Q75dM.js";import{I as H,D as S,H as j,a as v,b as h,S as z}from"./common-BOTKOHpD.js";import{R as t}from"./react-virtualized---iyVwns.js";import"./index-DRjF_FHU.js";import"./chunk-TVFJBHBT-BH-Rv9Ux.js";import"./assertThisInitialized-DUTgmdVG.js";import"./AutoSizer-i8YFPKNy.js";import"./defineProperty-EJDAiRQC.js";import"./objectWithoutPropertiesLoose-CAYKN5F1.js";const w={component:t,decorators:[r=>e.jsx("div",{style:{height:"100vh"},children:e.jsx(r,{})})]},n={render:()=>e.jsx(t,{count:1e4,Component:H})},o={render:()=>e.jsx(t,{count:1e4,Component:S})},s={render:()=>e.jsx(t,{count:1e4,Component:j})},a={render:()=>e.jsx(t,{count:1e4,Component:v})},c={render:()=>e.jsx(t,{count:1e4,Component:h})},m={render:()=>e.jsx(t,{count:1e6,Component:z})};var i,u,O;n.parameters={...n.parameters,docs:{...(i=n.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={ItemWithRenderCount} />;
+  }
+}`,...(O=(u=n.parameters)==null?void 0:u.docs)==null?void 0:O.source}}};var p,d,C;o.parameters={...o.parameters,docs:{...(p=o.parameters)==null?void 0:p.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={DynamicItem} />;
+  }
+}`,...(C=(d=o.parameters)==null?void 0:d.docs)==null?void 0:C.source}}};var R,_,W;s.parameters={...s.parameters,docs:{...(R=s.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={HeavyDOMItem} />;
+  }
+}`,...(W=(_=s.parameters)==null?void 0:_.docs)==null?void 0:W.source}}};var N,T,U;a.parameters={...a.parameters,docs:{...(N=a.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={HeavyJsItem} />;
+  }
+}`,...(U=(T=a.parameters)==null?void 0:T.docs)==null?void 0:U.source}}};var l,y,I;c.parameters={...c.parameters,docs:{...(l=c.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={DynamicImageItem} />;
+  }
+}`,...(I=(y=c.parameters)==null?void 0:y.docs)==null?void 0:I.source}}};var g,D,x;m.parameters={...m.parameters,docs:{...(g=m.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 1000000;
+    return <ReactVirtualizedList count={ROW_COUNT} Component={SimpleItem} />;
+  }
+}`,...(x=(D=m.parameters)==null?void 0:D.docs)==null?void 0:x.source}}};const A=["RenderCount","DynamicHeight","HeavyDOM","HeavyJS","DynamicImage","OneMillion"];export{o as DynamicHeight,c as DynamicImage,s as HeavyDOM,a as HeavyJS,m as OneMillion,n as RenderCount,A as __namedExportsOrder,w as default};
