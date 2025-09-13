87029327327d1f797bd0c88eb24f8daddd586d5e
inokawa <48897392+inokawa@users.noreply.github.com>
2025-09-14 01:10:20 +0900

    Replace @storybook/test-runner

---
+++ .storybook/vitest.setup-solid.ts
@@ -0,0 +1,14 @@
+import { setProjectAnnotations } from "storybook-solidjs-vite";
+import * as projectAnnotations from "./preview";
+
+// https://github.com/inokawa/virtua#what-is-resizeobserver-loop-completed-with-undelivered-notifications-error
+const onError = (e: ErrorEvent) => {
+  if (e.message.includes("ResizeObserver")) {
+    e.stopImmediatePropagation();
+  }
+};
+window.addEventListener("error", onError);
+
+// This is an important step to apply the right configuration when testing your stories.
+// More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
+setProjectAnnotations([projectAnnotations]);
