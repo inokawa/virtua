268af5948545eee27d897af009097898db266a80
inokawa <48897392+inokawa@users.noreply.github.com>
2025-10-07 20:30:45 +0900

    Reduce contain style to fix browser-specific edge case bugs (#775)
    * Reduce contain style to fix browser-specific edge case bugs
    
    * wip

--- src/svelte/Virtualizer.svelte
+++ src/svelte/Virtualizer.svelte
@@ -135,7 +135,7 @@
 
   let containerStyle = $derived(
     styleToString({
-      contain: "strict",
+      contain: "size paint style", // https://github.com/inokawa/virtua/pull/775
       "overflow-anchor": "none", // opt out browser's scroll anchoring because it will conflict to scroll anchoring of virtualizer
       overflow: "clip", // https://github.com/inokawa/virtua/pull/485 https://github.com/inokawa/virtua/issues/717
       flex: "none", // flex style can break layout
--- src/vue/Virtualizer.tsx
+++ src/vue/Virtualizer.tsx
@@ -299,7 +299,7 @@ export const Virtualizer = /*#__PURE__*/ defineComponent({
         <Element
           ref={containerRef}
           style={{
-            contain: "strict",
+            contain: "size paint style", // https://github.com/inokawa/virtua/pull/775
             overflowAnchor: "none", // opt out browser's scroll anchoring because it will conflict to scroll anchoring of virtualizer
             overflow: "clip", // https://github.com/inokawa/virtua/pull/485 https://github.com/inokawa/virtua/issues/717
             flex: "none", // flex style can break layout
