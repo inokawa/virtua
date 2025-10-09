461f17a207f722de7e00d59371936a6e8048f2ad
inokawa <inokawa@users.noreply.github.com>
2025-10-09 03:41:24 +0000

    deploy: 0ee57ed6d2c0e86eff4d759e5307a1fc6650f15b

---
+++ assets/NewWindow.stories-Dp6mqr2_.js
@@ -0,0 +1,18 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{r as i}from"./iframe-Bb63iTYX.js";import{r as p}from"./index-DbtEPA8V.js";import{V as c}from"./VList-BVkJEiNE.js";import"./preload-helper-PPVm8Dsz.js";import"./index-BUt4b3i1.js";import"./Virtualizer-B21aO86c.js";import"./useLatestRef-CPHvhl0P.js";import"./useChildren-UqILbHoM.js";const E={component:c},l=e=>{const n=[20,40,80,77];return Array.from({length:e}).map((o,r)=>t.jsx("div",{style:{height:n[r%4],borderBottom:"solid 1px #ccc",background:"#fff"},children:r},r))},u=({children:e,onUnload:n})=>{const[o,r]=i.useState(null);return i.useLayoutEffect(()=>{const s=window.open("","","width=400,height=400,left=200,top=200");if(!s)return;const a=s.document.createElement("div");return s.document.body.appendChild(a),s.addEventListener("unload",n,{once:!0}),r(a),()=>{s?.close()}},[]),o?p.createPortal(e,o):null},m=()=>t.jsx(c,{style:{height:"100vh"},children:l(1e3)}),d={name:"NewWindow",render:()=>{const[e,n]=i.useState(!1);return t.jsxs("div",{children:[t.jsxs("button",{onClick:()=>{n(o=>!o)},children:[e?"close":"open"," window"]}),e&&t.jsx(u,{onUnload:()=>{n(!1)},children:t.jsx(m,{})})]})}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  name: "NewWindow",
+  render: () => {
+    const [isWindowOpened, setIsWindowOpened] = useState(false);
+    return <div>
+        <button onClick={() => {
+        setIsWindowOpened(prev => !prev);
+      }}>
+          {isWindowOpened ? "close" : "open"} window
+        </button>
+        {isWindowOpened && <NewWindow onUnload={() => {
+        setIsWindowOpened(false);
+      }}>
+            <Content />
+          </NewWindow>}
+      </div>;
+  }
+}`,...d.parameters?.docs?.source}}};const b=["Default"];export{d as Default,b as __namedExportsOrder,E as default};
---
+++ assets/index-DbtEPA8V.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-Bb63iTYX.js";import{r as o}from"./index-BUt4b3i1.js";var t=o();const m=r(t);export{m as P,t as r};
