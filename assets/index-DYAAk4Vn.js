7e01c68d67465916091663bb719d84f4dd778163
inokawa <inokawa@users.noreply.github.com>
2025-10-10 15:44:09 +0000

    deploy: fd7722c6baa554ec43802633bb88d3fce7bf8c00

---
+++ assets/index-DYAAk4Vn.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-BfTLd5pu.js";import{r as o}from"./index-CEX4BQNH.js";var t=o();const m=r(t);export{m as P,t as r};
---
+++ assets/react-window.stories-QhhKYUig.js
@@ -0,0 +1,31 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{I as i,D as O,H as d,a as p,b as u,S as C}from"./common-B9SLdhHd.js";import{R as r}from"./react-window-DZ1GaF_r.js";import"./iframe-BfTLd5pu.js";import"./preload-helper-PPVm8Dsz.js";import"./chunk-4X5ZEQ5K-DKZdp5Hc.js";const y={component:r,decorators:[n=>e.jsx("div",{style:{height:"100vh"},children:e.jsx(n,{})})]},t={render:()=>e.jsx(r,{count:1e4,Component:i})},o={render:()=>e.jsx(r,{count:1e4,Component:O})},s={render:()=>e.jsx(r,{count:1e4,Component:d})},a={render:()=>e.jsx(r,{count:1e4,Component:p})},c={render:()=>e.jsx(r,{count:1e4,Component:u})},m={render:()=>e.jsx(r,{count:1e6,Component:C})};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactWindowList count={ROW_COUNT} Component={ItemWithRenderCount} />;
+  }
+}`,...t.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactWindowList count={ROW_COUNT} Component={DynamicItem} />;
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactWindowList count={ROW_COUNT} Component={HeavyDOMItem} />;
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactWindowList count={ROW_COUNT} Component={HeavyJsItem} />;
+  }
+}`,...a.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 10000;
+    return <ReactWindowList count={ROW_COUNT} Component={DynamicImageItem} />;
+  }
+}`,...c.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  render: () => {
+    const ROW_COUNT = 1000000;
+    return <ReactWindowList count={ROW_COUNT} Component={SimpleItem} />;
+  }
+}`,...m.parameters?.docs?.source}}};const I=["RenderCount","DynamicHeight","HeavyDOM","HeavyJS","DynamicImage","OneMillion"];export{o as DynamicHeight,c as DynamicImage,s as HeavyDOM,a as HeavyJS,m as OneMillion,t as RenderCount,I as __namedExportsOrder,y as default};
