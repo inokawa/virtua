a55ec4df15ca53afc68ef49b9228a81c1a60d3a0
inokawa <inokawa@users.noreply.github.com>
2025-10-11 09:44:16 +0000

    deploy: cb302e3f486df2598a08c73194aca97575ded23a

---
+++ assets/index-BfGwGeRy.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-BzEk9ymh.js";import{r as o}from"./index-CBPQlJgY.js";var t=o();const m=r(t);export{m as P,t as r};
--- assets/index-DYAAk4Vn.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-BfTLd5pu.js";import{r as o}from"./index-CEX4BQNH.js";var t=o();const m=r(t);export{m as P,t as r};
