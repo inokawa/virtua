5902182df43cd344eb5e775581781260e82047b7
alexeyvokin <alvo14@mail.ru>
2025-10-13 13:50:19 +0700

    fix: use the requestAnimationFrame method instead of microtask

--- src/vue/Virtualizer.tsx
+++ src/vue/Virtualizer.tsx
@@ -26,7 +26,6 @@ import {
   createScroller,
   ItemsRange,
   ScrollToIndexOpts,
-  microtask,
   sort,
 } from "../core/index.js";
 import { ListItem } from "./ListItem.js";
@@ -192,7 +191,7 @@ export const Virtualizer = /*#__PURE__*/ defineComponent({
     onMounted(() => {
       isSSR = false;
 
-      microtask(() => {
+      const raf = requestAnimationFrame(() => {
         const assignScrollableElement = (e: HTMLElement) => {
           resizer.$observeRoot(e);
           scroller.$observe(e);
@@ -203,6 +202,10 @@ export const Virtualizer = /*#__PURE__*/ defineComponent({
         } else {
           assignScrollableElement(containerRef.value!.parentElement!);
         }
+      });
+
+      onUnmounted(() => {
+        cancelAnimationFrame(raf);
       });
     });
     onUnmounted(() => {
