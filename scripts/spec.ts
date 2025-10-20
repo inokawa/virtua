ebc784f571549aabe4dc682f4302dbc41e23a27b
inokawa <48897392+inokawa@users.noreply.github.com>
2025-10-20 18:28:17 +0900

    Update spec.ts

--- scripts/spec.ts
+++ scripts/spec.ts
@@ -17,17 +17,7 @@ export const setupResizeJsDom = ({
     { width, height }: { width: number; height: number }
   ): ResizeObserverEntry => {
     const entry: Pick<ResizeObserverEntry, "contentRect" | "target"> = {
-      contentRect: {
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        width: width,
-        height: height,
-        x: 0,
-        y: 0,
-        toJSON() {},
-      },
+      contentRect: new DOMRect(0, 0, width, height),
       target: e,
     };
     return entry as any;
