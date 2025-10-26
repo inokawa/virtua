e85d52d15ee5720d2d58d487f4ec90dc220519e0
inokawa <48897392+inokawa@users.noreply.github.com>
2025-10-27 02:19:07 +0900

    Refine props

--- src/react/VGrid.ssr.spec.tsx
+++ src/react/VGrid.ssr.spec.tsx
@@ -9,6 +9,37 @@ import { JSDOM } from "jsdom";
 const LIST_ID = "list-id";
 
 describe("SSR", () => {
+  it("should render nothing", () => {
+    const ROW_COUNT = 0;
+    const COL_COUNT = 0;
+    const ITEM_SIZE = 40;
+    const BUFFER_SIZE = ITEM_SIZE * 4;
+    const html = renderToString(
+      <VGrid
+        id={LIST_ID}
+        ssrRowCount={ROW_COUNT}
+        ssrColCount={COL_COUNT}
+        cellHeight={ITEM_SIZE}
+        cellWidth={ITEM_SIZE}
+        bufferSize={BUFFER_SIZE}
+        row={1000}
+        col={5000}
+      >
+        {({ rowIndex, colIndex }) => (
+          <div>
+            {rowIndex}-{colIndex}
+          </div>
+        )}
+      </VGrid>
+    );
+    expect(html).toMatchSnapshot();
+
+    expect(
+      new JSDOM(html).window.document.getElementById(LIST_ID)!.children[0]!
+        .childElementCount
+    ).toEqual(ROW_COUNT * COL_COUNT);
+  });
+
   it("should render items with renderToString and vertical", () => {
     const ROW_COUNT = 10;
     const COL_COUNT = 20;
@@ -17,8 +48,8 @@ describe("SSR", () => {
     const html = renderToString(
       <VGrid
         id={LIST_ID}
-        initialRowCount={ROW_COUNT}
-        initialColCount={COL_COUNT}
+        ssrRowCount={ROW_COUNT}
+        ssrColCount={COL_COUNT}
         cellHeight={ITEM_SIZE}
         cellWidth={ITEM_SIZE}
         bufferSize={BUFFER_SIZE}
@@ -48,8 +79,8 @@ describe("SSR", () => {
     const html = renderToStaticMarkup(
       <VGrid
         id={LIST_ID}
-        initialRowCount={ROW_COUNT}
-        initialColCount={COL_COUNT}
+        ssrRowCount={ROW_COUNT}
+        ssrColCount={COL_COUNT}
         cellHeight={ITEM_SIZE}
         cellWidth={ITEM_SIZE}
         bufferSize={BUFFER_SIZE}
