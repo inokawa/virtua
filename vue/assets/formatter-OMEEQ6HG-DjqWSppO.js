d1534f30baa48ddc7009dda52c3ae9e119e12700
inokawa <inokawa@users.noreply.github.com>
2025-12-01 13:05:56 +0000

    deploy: 5f636367635122c52d80f39f1c37be1f61eb7670

---
+++ vue/assets/formatter-OMEEQ6HG-DjqWSppO.js
@@ -0,0 +1,1 @@
+import{d as i,a as t}from"./WithTooltip-SK46ZJ2J-k9iQ-NYV.js";import{g as m}from"./iframe-DNrFp9Sp.js";import"./preload-helper-PPVm8Dsz.js";var a=m(t(),1),d=(0,a.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
