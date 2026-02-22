d485cd588e53861c5a7815d1b6c851f740b0fbd2
inokawa <inokawa@users.noreply.github.com>
2026-02-22 17:21:13 +0000

    deploy: 2b2b3004436c5451a10209cca126ddc3eb854f92

---
+++ svelte/assets/formatter-QJ4M4OGQ-CmDEbrPU.js
@@ -0,0 +1,1 @@
+import{d as i,b as m}from"./DocsRenderer-GHJI37HO-01B0nk8g.js";import{b2 as o}from"./iframe-DhV1RiGE.js";import"./preload-helper-PPVm8Dsz.js";var t=o(m(),1),d=(0,t.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
