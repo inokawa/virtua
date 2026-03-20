4b640deae55a27286bc9cfb123819a6b5d5426ad
inokawa <48897392+inokawa@users.noreply.github.com>
2026-03-20 12:17:06 +0900

    Update storybook

--- .storybook/vitest.setup-react.ts
+++ .storybook/vitest.setup-react.ts
@@ -1,6 +1,3 @@
-import { setProjectAnnotations } from "@storybook/react-vite";
-import * as projectAnnotations from "./preview";
-
 // https://github.com/inokawa/virtua#what-is-resizeobserver-loop-completed-with-undelivered-notifications-error
 const onError = (e: ErrorEvent) => {
   if (e.message.includes("ResizeObserver")) {
@@ -8,7 +5,3 @@ const onError = (e: ErrorEvent) => {
   }
 };
 window.addEventListener("error", onError);
-
-// This is an important step to apply the right configuration when testing your stories.
-// More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-setProjectAnnotations([projectAnnotations]);
