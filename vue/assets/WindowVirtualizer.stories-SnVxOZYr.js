0bbac437c5d29242526757873480c5a7c56c141d
inokawa <inokawa@users.noreply.github.com>
2026-03-30 12:59:38 +0000

    deploy: 9c681e31a83b3fd8a7071755f1371bc1f6dcc04c

--- vue/assets/WindowVirtualizer.stories-SnVxOZYr.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-BneVvdWh.js";import{A as t,E as n,f as r,h as i,i as a,j as o,k as s,m as c,u as l,x as u}from"./iframe-C9RY6t2E.js";import{n as d,t as f}from"./vue-D40AGNY_.js";import{n as p,t as m}from"./_plugin-vue_export-helper-6nPRbwJT.js";var h,g,_,v=e((()=>{a(),f(),h={style:{padding:`200px 100px`}},g={style:{border:`solid 1px gray`}},_=i({__name:`WindowVirtualizer`,setup(e){let i=[20,40,180,77],a=Array.from({length:1e3}).map((e,t)=>i[t%4]);return(e,i)=>(u(),r(`div`,h,[l(`div`,g,[c(s(d),{data:s(a)},{default:n(({item:e,index:n})=>[(u(),r(`div`,{key:n,style:t({height:e+`px`,background:`white`,borderBottom:`solid 1px #ccc`})},o(n),5))]),_:1},8,[`data`])])]))}})})),y=e((()=>{})),b,x=e((()=>{v(),v(),y(),p(),b=m(_,[[`__scopeId`,`data-v-df1ac83b`]])})),S,C,w;e((()=>{f(),x(),S={component:d},C={render:()=>({components:{Component:b},template:`<Component />`})},C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`{
-  render: () => ({
-    components: {
-      Component: DefaultComponent
-    },
-    template: "<Component />"
-  })
-}`,...C.parameters?.docs?.source}}},w=[`Default`]}))();export{C as Default,w as __namedExportsOrder,S as default};
